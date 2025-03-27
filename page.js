e36919799e6fb943dee435ca59adcca5576991bf
Skahad <sahad3889@gmail.com>
2025-03-27 21:23:32 +0530

    completed

---
+++ page.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function(){
+
+    let navbar = this.document.getElementById("navbar");
+    if (this.window.scrollY > 50){
+        navbar.classList.add("scrolled");
+}
+else{
+    navbar.classList.remove("scrolled");
+
+}
+}
+
+);
