78f143407aa11584e67433723e3ad348ee04606e
Skahad <sahad3889@gmail.com>
2025-04-03 21:53:52 +0530

    updated

--- page.js
+++ page.js
@@ -1,13 +1,13 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
 
     let navbar = this.document.getElementById("navbar");
-    if (this.window.scrollY > 50){
+    if (this.window.scrollY > 50) {
         navbar.classList.add("scrolled");
-}
-else{
-    navbar.classList.remove("scrolled");
+    }
+    else {
+        navbar.classList.remove("scrolled");
 
-}
+    }
 }
 
 );
